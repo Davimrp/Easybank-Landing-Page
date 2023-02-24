0d070391a28806b1c950b61b49625a3e3713148a
Davimrp <daviwendell2015@gmail.com>
2023-02-24 20:44:27 -0300

    style: Responsividade

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -2,26 +2,34 @@ const botaoMenu = document.querySelector('.click--menu-mobile');
 const menuMobile = document.querySelector(".menu-mobile");
 const fundoEscuroDoMenu = document.querySelector('.menu-mobile--foco');
 const botaoSairDoMenu = document.querySelector('.menu-mobile--sair')
+const botoesMenu = menuMobile.querySelectorAll('a')
 
 
-function abrirMenu(){
-    menuMobile.classList.add('aparece');
-    fundoEscuroDoMenu.classList.add('aparece')
-    document.querySelector('body').style.overflow = 'hidden';
+function clickMenu(){
+    let teste = ''
+    for (let i of botoesMenu){
+            i.addEventListener('click', fecharMenu)
+        }
 }
-botaoMenu.addEventListener('click', abrirMenu)
-
 
 function fecharMenu(){
     menuMobile.classList.remove('aparece');
     fundoEscuroDoMenu.classList.remove('aparece')
     document.querySelector('body').style.overflow = '';
 }
-
 botaoSairDoMenu.addEventListener('click', fecharMenu)
 
-function teste(){
+function abrirMenu(){
+    menuMobile.classList.add('aparece');
+    fundoEscuroDoMenu.classList.add('aparece')
+    document.querySelector('body').style.overflow = 'hidden';
+
+    clickMenu()
+}
+botaoMenu.addEventListener('click', abrirMenu)
+
+
+function solucionaBugOverflow(){
     document.querySelector('body').style.overflowX = 'hidden';
 }
-
-teste()
+solucionaBugOverflow()
